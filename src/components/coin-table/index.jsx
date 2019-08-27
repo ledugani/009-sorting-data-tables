dd4191751bb32a913b2d46b1e3f4966fdc90d0e0
Tom Dugan <thomas@aadwebb.com>
2019-08-27 11:29:16 -0500

    fin

---
+++ src/components/coin-table/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function CoinTable(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  )
+}
